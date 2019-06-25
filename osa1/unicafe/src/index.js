f8f09e2cd5266b4433a3e2f20381640327c673f7
Artoiss <aarttu@windowslive.com>
2019-06-25 19:05:36 +0300

    1.7 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -12,44 +12,43 @@ const App = () => {
   // Headerit
   const text1 = ["Give feedback", "Statistics"]
 
+
   return (
     <div>
         <Header text={text1[0]} />
       <div>
-        <Button handle ={() => setGood(good+1)} text = "Good" />
-        <Button handle ={() => setNeutral(neutral+1)} text = "Neutral" />
-        <Button handle ={() => setBad(bad+1)} text = "Bad" />
+        <Button handle={() => setGood(good + 1)} text="Good" />
+        <Button handle={() => setNeutral(neutral+1)} text="Neutral" />
+        <Button handle={() => setBad(bad+1)} text="Bad" />
       </div>
         <Header text={text1[1]} />
-      <div>
-          Good {good}
-      </div>
-      <div>
-          Neutral {neutral}
-      </div>
-      <div>
-          Bad {bad}
-      </div>
-      <div>
-          All
-      </div>
-      <div>
-          Average
-      </div>
-      <div>
-          Positive
-      </div>
+        <Stats amount={good} text="Good"/>
+        <Stats amount={neutral} text="Neutral" />
+        <Stats amount={bad} text="Bad" />
+        <Stats amount={good+neutral+bad} text={"All"} />
+        <Stats amount={((good)*(1) + (neutral)*(0) + bad*(-1))/(good+bad+neutral)} text={"Average"} />
+        <Stats amount={((good)/(good+bad+neutral))*100} text={"Positive"} />
     </div>
   )
+}
+
+const Stats = (props) => {
+  if (props.text === "Positive"){
+    return <div> {props.text} {props.amount} % </div>
+  }
+  else{
+
+  }
+  return <div> {props.text} {props.amount} </div>
 }
 
 const Header = (props) => {
     return <h1> {props.text}</h1>
 }
 
-const Button = ({handle, text}) => (
-    <button onClick={handle}>
-          {text}
+const Button = (props) => (
+    <button onClick={props.handle}>
+          {props.text}
     </button>
 )
 
