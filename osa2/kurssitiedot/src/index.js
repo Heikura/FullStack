435abfe6a62ccd8a95da1d9c48b419e77d060ba0
Artoiss <aarttu@windowslive.com>
2019-08-22 20:16:59 +0300

    2.1 toimii

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+
+  )
+}
+
+const Header = (props) => {
+  return <h1> {props.course} </h1>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part parts={props.parts[0]} />
+      <Part parts={props.parts[1]} />
+      <Part parts={props.parts[2]} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return <p> {props.parts.name} {props.parts.exercises} </p>
+}
+
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
